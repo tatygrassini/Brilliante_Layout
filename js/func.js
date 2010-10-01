2baf2255789ced7df5486161662880eb7db4328c
tatygrassini <tatygrassini@gmail.com>
2010-10-01 02:38:16 +0200

    Screenshot file added

---
+++ js/func.js
@@ -0,0 +1,5 @@
+Cufon.replace('h3');
+Cufon.replace('h4');
+Cufon.replace('.cat a'); // Requires a selector engine for IE 6-7
+Cufon.replace('.links a'); // Requires a selector engine for IE 6-7
+Cufon.replace('.twitter p.cufon'); // Requires a selector engine for IE 6-7
