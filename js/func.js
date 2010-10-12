8e7af27bfb529c8518beaad05fd0e384276f7fb0
tatygrassini <tatygrassini@gmail.com>
2010-10-13 01:38:36 +0200

    Nav links done with cufon - selected state to go

--- js/func.js
+++ js/func.js
@@ -3,3 +3,4 @@ Cufon.replace('h4');
 Cufon.replace('.cat a'); // Requires a selector engine for IE 6-7
 Cufon.replace('.links a'); // Requires a selector engine for IE 6-7
 Cufon.replace('.twitter p.cufon'); // Requires a selector engine for IE 6-7
+Cufon.replace('.top-content ul li a'); // Requires a selector engine for IE 6-7
