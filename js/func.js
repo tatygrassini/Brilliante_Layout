8c60ab911524401b27c66d2f3f4bf6c25e46fc3e
tatygrassini <tatygrassini@gmail.com>
2010-10-15 12:55:22 +0200

    Cufon applied to h2 tags, main section only

--- js/func.js
+++ js/func.js
@@ -4,3 +4,5 @@ Cufon.replace('.cat a'); // Requires a selector engine for IE 6-7
 Cufon.replace('.links a'); // Requires a selector engine for IE 6-7
 Cufon.replace('.twitter p.cufon'); // Requires a selector engine for IE 6-7
 Cufon.replace('.top-content ul li a'); // Requires a selector engine for IE 6-7
+Cufon.replace('.cufon-h2'); // Requires a selector engine for IE 6-7
+Cufon.replace('.cufon-h2-footer'); // Requires a selector engine for IE 6-7
