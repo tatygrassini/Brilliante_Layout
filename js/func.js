48a4b2a350eb01733946eecdcdba8fa99da6ac34
tatygrassini <tatygrassini@gmail.com>
2010-12-05 03:30:40 +0100

    Replaced image-generated white border line with CSS and croped image.

--- js/func.js
+++ js/func.js
@@ -1,7 +1,7 @@
 Cufon.replace('h3');
 Cufon.replace('h4');
-Cufon.replace('.cat a'); // Requires a selector engine for IE 6-7
-Cufon.replace('.links a'); // Requires a selector engine for IE 6-7
+Cufon.replace('.cat li'); // Requires a selector engine for IE 6-7
+Cufon.replace('.links li'); // Requires a selector engine for IE 6-7
 Cufon.replace('.twitter p.cufon'); // Requires a selector engine for IE 6-7
-Cufon.replace('.top-content a'); // Requires a selector engine for IE 6-7
+Cufon.replace('.top-content li'); // Requires a selector engine for IE 6-7
 Cufon.replace('h2'); // Requires a selector engine for IE 6-7
