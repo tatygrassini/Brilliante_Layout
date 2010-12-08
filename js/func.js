2bcfa299c33bc3a35a64c83029b769c2bdac0bad
tatygrassini <tatygrassini@gmail.com>
2010-12-08 04:51:35 +0100

    Thanks jQuery, now iE is rendering Cufon-styled links properly.

--- js/func.js
+++ js/func.js
@@ -1,7 +1,7 @@
 Cufon.replace('h3');
 Cufon.replace('h4');
-Cufon.replace('.cat li'); // Requires a selector engine for IE 6-7
-Cufon.replace('.links li'); // Requires a selector engine for IE 6-7
-Cufon.replace('.twitter p.cufon'); // Requires a selector engine for IE 6-7
-Cufon.replace('.top-content li'); // Requires a selector engine for IE 6-7
-Cufon.replace('h2'); // Requires a selector engine for IE 6-7
+Cufon.replace('.cat li a');
+Cufon.replace('.links li a');
+Cufon.replace('.twitter p.cufon');
+Cufon.replace('.top-content li');
+Cufon.replace('h2');
