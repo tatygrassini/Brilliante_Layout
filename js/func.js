b67f56f18476dae7d736c6671d6608f38e8d74f4
tatygrassini <tatygrassini@gmail.com>
2010-12-12 04:16:02 +0100

    Minified CSS and JS files.

--- js/func.js
+++ js/func.js
@@ -1,7 +1,2 @@
-Cufon.replace('h3');
-Cufon.replace('h4');
-Cufon.replace('.cat li a');
-Cufon.replace('.links li a');
-Cufon.replace('.twitter p.cufon');
-Cufon.replace('.top-content li');
+Cufon.replace('h3, h4, .cat li a, .links li a, .twitter p.cufon, .top-content li');
 Cufon.replace('h2');
