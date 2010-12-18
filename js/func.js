7e5b4cd622e2168eb218412fe26423c97f65f845
tatygrassini <tatygrassini@gmail.com>
2010-12-18 03:13:59 +0100

    Alt tags completed, better markup, CSS cleaning

--- js/func.js
+++ js/func.js
@@ -1,2 +1,5 @@
-Cufon.replace('h3, h4, .cat li a, .links li a, .twitter p.cufon, .top-content li');
+Cufon.replace('h3, h4, .twitter p.cufon, .top-content li');
+
+Cufon.replace('.cat li a, .links li a', {hover: 'true', textDecoration:'underline'});
+
 Cufon.replace('h2');
