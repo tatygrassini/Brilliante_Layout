9555c08d664dfe5125e1bd74b175ad52a3221aeb
tatygrassini <tatygrassini@gmail.com>
2011-06-01 01:10:59 +0200

    ReadMore button img replaced with CSS3, cleaner script and link tags in the mark up.

--- js/func.js
+++ js/func.js
@@ -4,6 +4,7 @@ Cufon.replace('h3, h4, .twitter p.cufon, .top-content li');
 Cufon.replace('.cat li a, .links li a', {hover: 'true', textDecoration:'underline'});
 Cufon.replace('h2');
 
+
 // -- slidejs ------------------------------- 
 
 			$(function(){
